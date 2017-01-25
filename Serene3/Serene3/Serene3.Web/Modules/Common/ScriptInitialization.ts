096cb34e7a03582ed49869930fecadc0bbb31a78
rlennox <ryanlennox@fathead.com>
2017-01-25 10:21:59 -0500

    Serenity test commit 1

---
+++ Serene3/Serene3/Serene3.Web/Modules/Common/ScriptInitialization.ts
@@ -0,0 +1,5 @@
+﻿namespace Serene3.ScriptInitialization {
+    Q.Config.responsiveDialogs = true;
+    Q.Config.rootNamespaces.push('Serene3');
+    Serenity.EntityDialog.defaultLanguageList = LanguageList.getValue;
+}
